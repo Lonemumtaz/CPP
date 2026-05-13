9b491b4478da3eb14d0929b2064c680e874300c4
lonemumtaz <mumtazlone1315@gmail.com>
2026-05-13 17:41:04 +0530

    binarytree

---
+++ binary_tree.cpp
@@ -0,0 +1,37 @@
+// binary_tree.cpp
+// Basic binary tree implementation in C++
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newNode(int data) {
+    Node* node = new Node();
+    node->data = data;
+    node->left = node->right = nullptr;
+    return node;
+}
+
+void inorder(Node* root) {
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    cout << "Inorder Traversal: ";
+    inorder(root);
+    cout << endl;
+    return 0;
+}
